687ba7c4311d92a0263e5b2ffca39015a588ff2a
unknown <TINE16>
2025-07-07 10:00:11 +0800

    gg

--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,76 @@
+<template>
+    <div class="d-flex justify-items-center align-center">
+  
+     
+  
+      <v-card
+        class="mx-auto pa-12 pb-8"
+        elevation="8"
+        max-width="448"
+        rounded="lg"
+      >
+        <div class="text-subtitle-1">Login</div>
+  
+        <v-text-field
+          density="compact"
+          placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
+  
+        <div class="text-subtitle-2 text-medium-emphasis d-flex align-center justify-space-between">
+          Password
+  
+        
+        </div>
+  
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+  
+        <v-card
+          class="mb-12"
+          color="surface-variant"
+          variant="tonal"
+        >
+          
+        </v-card>
+  
+        <v-btn
+          class="mb-8"
+          color="blue"
+          size="large"
+          variant="tonal"
+          block
+        >
+          sign-in 
+        </v-btn>
+  
+        <v-card-text class="text-center">
+          <a
+            class="text-blue text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-card>
+    </div>
+  </template>
+  
+  <style>
+  .text-subtitle-1 {
+  margin-left: 150px;
+  }
+  </style>
 <script>
 export default{
     layout:"auth",
